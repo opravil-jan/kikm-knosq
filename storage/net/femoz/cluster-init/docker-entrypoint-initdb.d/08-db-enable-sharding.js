b9a2f19510dd407f0c0395742302a32d0a81b2c3
Jan Opravil <jan.opravil@femoz.cz>
2026-01-29 05:58:58 +0100

    fix: problem with unique index when importing data

--- storage/net/femoz/cluster-init/docker-entrypoint-initdb.d/08-db-enable-sharding.js
+++ storage/net/femoz/cluster-init/docker-entrypoint-initdb.d/08-db-enable-sharding.js
@@ -1,6 +1,12 @@
-db = db.getSiblingDB('video_watch_time') 
+db = db.getSiblingDB("video_watch_time");
+
 sh.enableSharding("video_watch_time");
-db.devices.createIndex({ deviceId: "hashed" })
+
+db.devices.createIndex({ deviceId: "hashed" });
 sh.shardCollection("video_watch_time.devices", { deviceId: "hashed" });
-db.viewers.createIndex({ userId: "hashed" })
+
+db.viewers.createIndex({ userId: "hashed" });
 sh.shardCollection("video_watch_time.viewers", { userId: "hashed" });
+
+db.devices.createIndex({ deviceId: 1, idec: 1 }, { unique: true });
+db.viewers.createIndex({ userId: 1, idec: 1 }, { unique: true });
