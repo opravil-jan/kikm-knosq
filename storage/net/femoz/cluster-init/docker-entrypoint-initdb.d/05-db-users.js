992f58078d0a1faf9a1e52d5f980a4c609ba48f0
Jan Opravil <jan.opravil@femoz.cz>
2026-01-29 12:18:11 +0100

    fix: file order

--- storage/net/femoz/cluster-init/docker-entrypoint-initdb.d/05-db-users.js
+++
@@ -1,5 +0,0 @@
-db.createUser({
-  user: "pcm",
-  pwd: "woo4rae9Iepopeithoor1quieYo6Yai0", // or cleartext password
-  roles: [{ role: "readWrite", db: "video_watch_time" }],
-});
---
+++ storage/net/femoz/cluster-init/docker-entrypoint-initdb.d/06-db-users.js
@@ -0,0 +1,15 @@
+db = db.getSiblingDB("video_watch_time");
+db.createUser({
+  user: "pcm",
+  pwd: "woo4rae9Iepopeithoor1quieYo6Yai0",
+  roles: [{ role: "readWrite", db: "video_watch_time" }],
+});
+
+db = db.getSiblingDB("admin");
+db.createUser({
+  user: "clusterAdmin",
+  pwd: "gaetaiNgaisou6eilahmoS6chahr3ohf",
+  roles: [{ role: "root", db: "admin" }],
+});
+
+db = db.getSiblingDB("video_watch_time");
